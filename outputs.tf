feddf10d490be192e2d104807f3bdef9a169a97d
JaydenK <84843818+jaydenk1@users.noreply.github.com>
2021-10-25 21:50:25 +1300

    Delete outputs.tf

--- outputs.tf
+++
@@ -1,8 +0,0 @@
-output "tls_private_key" {
-  value     = tls_private_key.example_ssh.private_key_pem
-  sensitive = true
-}
-
-output "public_ip_address" {
-  value = azurerm_public_ip.public_ip.ip_address
-}
